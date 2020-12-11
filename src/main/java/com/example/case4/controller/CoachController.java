07a391bb533e8ee06851f0d7c452020c449814e5
Chung <truongmanhchung207@gmail.com>
2020-12-11 08:13:50 +0700

    add

--- src/main/java/com/example/case4/controller/CoachController.java
+++ src/main/java/com/example/case4/controller/CoachController.java
@@ -1,5 +1,6 @@
 package com.example.case4.controller;
 
+import com.example.case4.model.Classroom;
 import com.example.case4.service.classroom.IClassService;
 import com.example.case4.service.coach.ICoachService;
 import com.example.case4.service.student.IStudentService;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/coach")
 public class CoachController {
@@ -20,11 +23,15 @@ public class CoachController {
     private ICoachService coachService;
     @Autowired
     private IStudentService studentService;
+    @Autowired
+    private IClassService classService;
 
     @GetMapping("/classlist/{id}")
     public ModelAndView showListClass(@PathVariable Long id){
         ModelAndView modelAndView = new ModelAndView("classroom");
         modelAndView.addObject("listStudent", studentService.getListClass(id));
+        Optional<Classroom> classroom = classService.findById(id);
+        modelAndView.addObject("classname",classroom.get().getName());
         return modelAndView;
     }
 
