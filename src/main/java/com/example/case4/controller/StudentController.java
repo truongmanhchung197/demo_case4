a6b8a9df4f749ab137a59b55e02d92c5b6589dec
conghoan21 <conghoan.hnc@gmail.com>
2020-12-11 11:07:17 +0700

    add file

--- src/main/java/com/example/case4/controller/StudentController.java
+++ src/main/java/com/example/case4/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.case4.controller;
 import com.example.case4.model.Student;
 import com.example.case4.service.student.IStudentService;
 import com.example.case4.service.student.StudentService;
+import com.example.case4.service.user.IAppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,4 +16,16 @@ import java.util.Optional;
 public class StudentController {
     @Autowired
     private IStudentService studentService;
+    @Autowired
+    private IAppUserService appUserService;
+    @GetMapping("/sutdent/{id}")
+    public ModelAndView showInfostudent(@PathVariable Long id){
+        ModelAndView modelAndView = new ModelAndView("homeStudent");
+        modelAndView.addObject("student", appUserService.getCurrentUserId());
+        Optional<Student>student=studentService.findById(id);
+        modelAndView.addObject("student", student.get());
+        return modelAndView;
+    }
+}
+
 
