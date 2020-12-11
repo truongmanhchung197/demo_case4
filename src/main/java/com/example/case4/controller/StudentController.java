15ef0f22435ecb3f150e83e06e4767ca73c4bddb
conghoan21 <conghoan.hnc@gmail.com>
2020-12-11 09:41:39 +0700

    add file

--- src/main/java/com/example/case4/controller/StudentController.java
+++ src/main/java/com/example/case4/controller/StudentController.java
@@ -1,16 +1,18 @@
 package com.example.case4.controller;
 
+import com.example.case4.model.Student;
+import com.example.case4.service.student.IStudentService;
 import com.example.case4.service.student.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    private IStudentService studentService;
 
-
-}
