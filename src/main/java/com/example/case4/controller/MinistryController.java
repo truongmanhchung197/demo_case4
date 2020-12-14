8922e624b0d29ff82bfc325dfea0db32601426eb
conghoan21 <conghoan.hnc@gmail.com>
2020-12-14 15:10:47 +0700

    add file

--- src/main/java/com/example/case4/controller/MinistryController.java
+++ src/main/java/com/example/case4/controller/MinistryController.java
@@ -9,10 +9,7 @@ import com.example.case4.service.ministry.IMinistryService;
 import com.example.case4.service.student.IStudentService;
 import com.example.case4.service.user.IAppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
@@ -32,18 +29,18 @@ public class MinistryController {
     private IMarkStudentService markStudentService;
 
     @GetMapping("/classlist")
-    public ModelAndView showListClass(){
+    public ModelAndView showListClass() {
         ModelAndView modelAndView = new ModelAndView("classroom");
         Long id = appUserService.getCurrentUserId();
-        modelAndView.addObject("listClass",classService.findAll());
+        modelAndView.addObject("listClass", classService.findAll());
         return modelAndView;
     }
-//    @GetMapping("/classlist/{id}")
-//    public ModelAndView showStudent(@PathVariable Long id){
-//        ModelAndView modelAndView = new ModelAndView("listStudent");
-//        modelAndView.addObject("listClass", classService.findById(id));
-//        Optional<Student>student=studentService.findById(id);
-//        modelAndView.addObject("student", student.get());
-//        return modelAndView;
-//    }
+
+    @GetMapping("/classlist/{id}")
+    public ModelAndView showStudent(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("listStudent");
+        Iterable<Student> listStudent = studentService.getListClass(id);
+        modelAndView.addObject("listStudent", listStudent);
+        return modelAndView;
+    }
 }
